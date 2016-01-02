c689066fde741a09b6c856af14b380ec9514b416
Jeremy Raines <jraines@gmail.com>
2016-01-02 11:54:14 -0800

    Set up RSpec, remove specs for nonexistant helpers, smoke test

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  expect {subject}.to have_many :users
+  it 'has many users' do
+    t = Company.reflect_on_association(:users)
+    expect(t.macro).to eq :has_many
+  end
 end
